827094d7314791c8662a018ad855258e05e5ffbc
subhadarshini7 <90782853+subhadarshini7@users.noreply.github.com>
2026-04-29 12:41:45 +0530

    Create RewardService.java

---
+++ src/main/java/com/Reward/service/RewardService.java
@@ -0,0 +1,8 @@
+package com.yourname.rewards.service;
+
+import com.yourname.rewards.dto.RewardResponse;
+import java.time.LocalDate;
+
+public interface RewardService {
+    RewardResponse calculateRewards(Long customerId, LocalDate startDate, LocalDate endDate);
+}
